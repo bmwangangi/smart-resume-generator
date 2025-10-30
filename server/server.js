f4e8e13d9583a80bd631c82056af4377c9fa4da1
bartasa <trabbart8@gmail.com>
2025-10-30 16:46:34 +0300

    enabling backend access

--- server/server.js
+++ server/server.js
@@ -6,27 +6,34 @@ import userRouter from "./routes/userRoutes.js";
 import resumeRouter from "./routes/resumeRoutes.js";
 import aiRouter from "./routes/aiRoutes.js";
 import dotenv from "dotenv";
-dotenv.config();
 
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Database Connection
-await connectDB()
+await connectDB();
 
-app.use(express.json())
+// Parse JSON
+app.use(express.json());
+
+// CORS configuration
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    "http://localhost:5173", // local dev
+    "https://smart-resume-generator-653y.vercel.app" // deployed frontend
+  ],
   credentials: true,
-}))
+}));
 
+// Routes
+app.get('/', (req, res) => res.send("Server is live..."));
+app.use('/api/users', userRouter);
+app.use('/api/resumes', resumeRouter);
+app.use('/api/ai', aiRouter);
 
-app.get('/', (req, res)=>res.send("Server is live..."))
-app.use('/api/users', userRouter)
-app.use('/api/resumes', resumeRouter )
-app.use('/api/ai', aiRouter)
-
-app.listen(PORT, ()=>{
-  console.log(`server is running on port ${PORT}`)
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
